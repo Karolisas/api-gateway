8f9371e21304cfc7dce083eb61fcc7f815f23e75
karolis.gulbinas <karolisas@gmail.com>
2022-03-01 17:26:58 +0200

    init

---
+++ src/main/java/lt/karolis/microservices28/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package lt.karolis.microservices28.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/karolis/microservices28/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.karolis.microservices28.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
